5ce7aaeb2f98b429851cceab224c082d14ea8cd5
mlampard <mlampard@spotx.tv>
2019-03-10 19:22:17 +0000

    Need to write Div tests

---
+++ src/sml/instructions/DivInstruction.java
@@ -0,0 +1,60 @@
+package sml.instructions;
+
+import sml.Instruction;
+import sml.Machine;
+
+/**
+ * This class represents the Sub instruction from the language.
+ *
+ *  @author ...
+ */
+public class DivInstruction extends Instruction {
+
+    private int result;
+    private int op1;
+    private int op2;
+
+    /**
+     * @param label of the instruction
+     * @param op    the operands
+     */
+    public AddInstruction(String label, String op) {
+        super(label, op);
+    }
+
+    /**
+     * @param label     of the instruction
+     * @param res       of the computation
+     * @param operator1 the first operand
+     * @param operator2 the second operand
+     */
+    public AddInstruction(String label, int res, int operator1, int operator2) {
+        this(label, "div");
+        this.result = res;
+        this.op1 = operator1;
+        this.op2 = operator2;
+    }
+
+    /**
+     * Execute the instruction, probably modifying the registers.
+     *
+     * @param m the machine under which the instruction executes
+     */
+    @Override
+    public void execute(Machine m) {
+        int value1 = m.getRegisters().getRegister(op1);
+        int value2 = m.getRegisters().getRegister(op2);
+        m.getRegisters().setRegister(result, value1 / value2);
+    }
+
+    /**
+     * String representation of the instruction
+     *
+     * @return representation of the operands and result
+     */
+    @Override
+    public String toString() {
+        return super.toString() + " " + op1 + " / " + op2 + " to " + result;
+    }
+
+}
