11675aac60c0a66b47e9fcc621099128bee37cce
mlampard <mlampard@spotx.tv>
2019-03-11 20:52:25 +0000

    Lets write tests

---
+++ src/sml/instructions/BnzInstructionTest.java
@@ -0,0 +1,16 @@
+package sml.instructions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BnzInstructionTest {
+
+    @Test
+    public void execute() {
+    }
+
+    @Test
+    public void toString() {
+    }
+}
